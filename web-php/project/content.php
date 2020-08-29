a66033cf0c4207a01ab23e802fadf517cee61bbf
RizkyFahrizal <68326135+RizkyFahrizal@users.noreply.github.com>
2020-08-29 13:31:13 +0700

    pemmrograman web dan mobile minggu ke7 (php)
    membuat program dasar php minggu ke 7

---
+++ web-php/project/content.php
@@ -0,0 +1,12 @@
+<?php 
+$titel="Blog Belajar PHP Smk - Coding Itu Mudah";
+
+$logo='Fahrizal';
+
+$nav=['Home'=>'home.php',
+'About'=>'isi-php/about.php',
+'product'=>'product.php',
+'Peta'=>'peta.php',
+'Service'=>'isi-php/service.php',
+'Contact'=>'contact.php'];
+?>
